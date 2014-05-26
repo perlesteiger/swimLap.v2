fe196e0c24d031b9990775990d6d946dac425ad5
EmilieN <emilie.navarro38@gmail.com>
2014-05-26 11:28:52 +0200

    Little updates

--- swimlap/model/fonctions.inc.php
+++ swimlap/model/fonctions.inc.php
@@ -1,19 +1,19 @@
 <?php
 // Connexion, sélection de la base de données
-//    function connect_bdd() {
-//            $host = 'localhost';
-//            $dbname = 'swimlapbd';
-//            $user = 'admin';
-//            $password = 'admin';
-//
-//            $dbconn = pg_connect("host=".$host."
-//                                  dbname=".$dbname."
-//                                  user=".$user."
-//                                  password=".$password)
-//                    or die('Connexion impossible : ' . pg_last_error());
-//
-//            return $dbconn;
-//    }
+/*    function connect_bdd() {
+            $host = 'localhost';
+            $dbname = 'swimlapbd';
+            $user = 'admin';
+            $password = 'admin';
+
+            $dbconn = pg_connect("host=".$host."
+                                  dbname=".$dbname."
+                                  user=".$user."
+                                  password=".$password)
+                    or die('Connexion impossible : ' . pg_last_error());
+
+            return $dbconn;
+    }*/
 
 // Connexion, sélection de la base de données
 function connect_bdd() {
--- swimlap/view/import_file.php
+++ swimlap/view/import_file.php
@@ -8,18 +8,4 @@
             <input type="submit" name="import" id="importBtn" class="button" value="Importer"/>
         </form>
     </div>
-    <div id="export_block" class="file_block">
-        <h4 class="title2">Exporter un fichier FFNEX</h4>
-        <form id="form_export" method="post" action="<?php echo MODEL;?>fonctions_request_form.php">
-            <input type="hidden" name="type_form" value="export"/>
-            <label for="export_compet">Sélectionner la compétition à exporter :</label><br/>
-            <select class="data_export" name="export_compet">
-                <?php foreach ($meetings as $meet) {
-                    $meet = explode('|', $meet);
-                    echo '<option value="'.$meet[4].'">'.$meet[0].'</option>';
-                } ?>
-            </select><br/>
-            <input type="submit" name="export" id="exportBtn" class="button" value="Exporter"/><br/>
-        </form>
-    </div>
 </div>
